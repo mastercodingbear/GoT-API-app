fed6d8ce6c9ff4f9451d0781eb6f7497425bb96c
honey bear <mastercodingbear@gmail.com>
2022-08-24 12:19:47 -0500

    Filter done

---
+++ src/components/FilterFields/FilterCulture.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Filter.css';
+
+type Props = {
+  culture: string;
+  onFilterChanged: (culture: string) => void;
+};
+
+const FilterCulture: React.FC<Props> = ({ culture, onFilterChanged }) => {
+  return (
+    <div className="filter-culture">
+      Filter culture by:{' '}
+      <input
+        type="text"
+        value={culture}
+        onChange={(e) => onFilterChanged(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default FilterCulture;
--- src/pages/CharacterTable.tsx
+++ src/pages/CharacterTable.tsx
@@ -5,11 +5,15 @@ import { Character } from '../state/types';
 import CharacterTableBody from '../components/CharacterTableBody';
 import Pagination from '../components/Pagination';
 import Loading from '../components/Loading';
+import FilterGender from '../components/FilterFields/FilterGender';
+import FilterCulture from '../components/FilterFields/FilterCulture';
 
 const CharacterTable: React.FC = () => {
   const queryParams = new URLSearchParams(useLocation().search);
   const queryPage = parseInt(queryParams.get('page') ?? '1');
   const queryPageSize = parseInt(queryParams.get('pagesize') ?? '25');
+  const queryCulture = parseInt(queryParams.get('culture') ?? '');
+  const queryGender = parseInt(queryParams.get('gender') ?? 'Any');
   let navigate = useNavigate();
 
   const [characters, setCharacters] = useState<Character[]>([]);
@@ -17,12 +21,25 @@ const CharacterTable: React.FC = () => {
   const [pageSize, setPageSize] = useState<number>(queryPageSize);
   const [totalPage, setTotalPage] = useState<number>(1);
   const [isLoading, setLoading] = useState<boolean>(false);
+  const [filterGender, setFilterGender] = useState<string>('');
+  const [filterCulture, setFilterCulture] = useState<string>('');
 
-  const getCharacters = useCallback(async () => {
+  useEffect(() => {
+    getCharacters(filterCulture, filterGender);
+  }, [page, pageSize, filterCulture, filterGender]);
+
+  const getCharacters = async (culture: string = '', gender: string = '') => {
     try {
       setLoading(true);
+      let requestParams = `page=${page}&pageSize=${pageSize}`;
+      if (culture !== '') {
+        requestParams += `&culture=${culture}`;
+      }
+      if (gender !== '') {
+        requestParams += `&gender=${gender}`;
+      }
       const response = await fetch(
-        `https://anapioficeandfire.com/api/characters?page=${page}&pageSize=${pageSize}`
+        `https://anapioficeandfire.com/api/characters?${requestParams}`
       ).then(async (res) => {
         const lastQuery = res.headers
           .get('link')
@@ -35,19 +52,14 @@ const CharacterTable: React.FC = () => {
         return await res.json();
       });
       setLoading(false);
-      navigate(`/characters?page=${page}&pageSize=${pageSize}`);
+      navigate(`/characters?${requestParams}`);
       setPage(page);
       setPageSize(pageSize);
       setCharacters(response);
     } catch (error) {
       console.error(error);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page, pageSize]);
-
-  useEffect(() => {
-    getCharacters();
-  }, [getCharacters]);
+  };
 
   const handleChangePage = (page: number) => {
     setPage(page);
@@ -55,6 +67,17 @@ const CharacterTable: React.FC = () => {
 
   const handleChangePageSize = (pageSize: number) => {
     setPageSize(pageSize);
+    setPage(1);
+  };
+
+  const handleChangeCulture = (culture: string) => {
+    console.log(culture);
+    setFilterCulture(culture);
+    setPage(1);
+  };
+
+  const handleChangeGender = (gender: string) => {
+    setFilterGender(gender);
     setPage(1);
   };
 
@@ -69,6 +92,16 @@ const CharacterTable: React.FC = () => {
           onPageChange={handleChangePage}
           onPageSizeChange={handleChangePageSize}
         />
+        <div className="filter-container">
+          <FilterGender
+            gender={filterGender}
+            onFilterChanged={handleChangeGender}
+          />
+          <FilterCulture
+            culture={filterCulture}
+            onFilterChanged={handleChangeCulture}
+          />
+        </div>
         <table>
           <thead>
             <tr>
